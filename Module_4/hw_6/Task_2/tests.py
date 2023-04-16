4de91329863c6f5e0b145c47aba77ed333d24c66
Garpusha <theflash@yandex.ru>
2023-04-16 21:49:40 +0300

    hw_6_2

--- Module_4/hw_6/Task_2/tests.py
+++ Module_4/hw_6/Task_2/tests.py
@@ -1,19 +1,31 @@
 from unittest import TestCase
-import configparser
 import main
-import requests
-
 
 
 class TestYandex(TestCase):
 
-    def test_yandex(self, yandex_, dir_name):
-        result = yandex_.make_dir(dir_name)
-        self.assertEqual(201, result, 'Error')
+    def test_yandex(self):
+        token = main.read_config('tokens.ini', 'Tokens', 'YandexToken')
+        my_yandex = main.YandexDisk(token)
+        dir_name = 'TestDir'
+        result = my_yandex.make_dir(dir_name)
+        try:
+            self.assertEqual(201, result)
+            print("\nFolder was created successfully")
+        except AssertionError:
+            match result:
+                case 409:
+                    print("\nError. Folder already exists")
+                case 401:
+                    print("\nError. You're not authorized to proceed")
+                case 404:
+                    print('\nError. Resource not found')
+                case 429:
+                    print('\nError. Too many requests')
+                case 503:
+                    print('\nError. Service is not available')
+                case 507:
+                    print('\nError. Not enough space')
+                case _:
+                    print("\n Unknown error")
         return
-
-
-mytest = TestYandex()
-token = main.read_config('tokens.ini', 'Tokens', 'YandexToken')
-my_yandex = main.YandexDisk(token)
-mytest.test_yandex(my_yandex, 'TestDir')
